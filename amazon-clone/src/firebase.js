6745ae5301d9ab1fa73e8177d80093dab09805ee
Kristof Richard Nagy <knagy3@jaguarlandrover.com>
2020-11-12 22:09:44 +0100

    subtotal in progress

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyChfZYK2MrTpzEv4oKYAzC2BB03H_WCoxQ",
+    authDomain: "clone-23527.firebaseapp.com",
+    databaseURL: "https://clone-23527.firebaseio.com",
+    projectId: "clone-23527",
+    storageBucket: "clone-23527.appspot.com",
+    messagingSenderId: "526203467591",
+    appId: "1:526203467591:web:32b94680323efbb352fe62",
+    measurementId: "G-365XY58EVD"
+  };
